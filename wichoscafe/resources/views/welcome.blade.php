71247d80eae3becddf35c41f6f27163e4f20c2b4
fish <re4lawliet@gmail.com>
2019-05-24 22:18:49 -0600

    24-5-2019 Login e Inicio

--- wichoscafe/resources/views/welcome.blade.php
+++ wichoscafe/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <img src="images/logoinicio.png">
                 </div>
 
                 <div class="links">
